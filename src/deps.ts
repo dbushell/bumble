6cdbe54527bad3e580233e20301a977854b0e655
David Bushell <david@dbushell.com>
2023-11-15 18:14:40 +0000

    remove unused dep

--- src/deps.ts
+++ src/deps.ts
@@ -4,4 +4,3 @@ export {deepMerge} from 'https://deno.land/std@0.205.0/collections/mod.ts';
 export * as acorn from 'npm:acorn@8.11.2';
 export * as svelte from 'npm:svelte@4.2.2/compiler';
 export {default as typescript} from 'npm:typescript@5.2.2';
-export type {CompilerOptions} from 'npm:typescript@5.2.2';
