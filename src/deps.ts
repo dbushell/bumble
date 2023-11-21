a0b2881cc9f14575ac6b4376c1faead902907a2f
David Bushell <david@dbushell.com>
2023-11-21 06:10:49 +0000

    bump deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export * as path from 'https://deno.land/std@0.206.0/path/mod.ts';
-export * as hex from 'https://deno.land/std@0.206.0/encoding/hex.ts';
-export {deepMerge} from 'https://deno.land/std@0.206.0/collections/mod.ts';
+export * as path from 'https://deno.land/std@0.207.0/path/mod.ts';
+export * as hex from 'https://deno.land/std@0.207.0/encoding/hex.ts';
+export {deepMerge} from 'https://deno.land/std@0.207.0/collections/mod.ts';
 export * as acorn from 'npm:acorn@8.11.2';
-export * as svelte from 'npm:svelte@4.2.3/compiler';
-export {default as typescript} from 'npm:typescript@5.2.2';
+export * as svelte from 'npm:svelte@4.2.7/compiler';
+export {default as typescript} from 'npm:typescript@5.3.2';
