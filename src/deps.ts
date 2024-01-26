89e47af04db191bdf1fdcb77e8a13e4ef256dba8
David Bushell <david@dbushell.com>
2024-01-26 04:56:36 +0000

    update deps / temporary patch

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export * as path from 'https://deno.land/std@0.208.0/path/mod.ts';
-export {existsSync} from 'https://deno.land/std@0.208.0/fs/exists.ts';
-export {ensureDirSync} from 'https://deno.land/std@0.208.0/fs/ensure_dir.ts';
+export * as path from 'https://deno.land/std@0.212.0/path/mod.ts';
+export {existsSync} from 'https://deno.land/std@0.212.0/fs/exists.ts';
+export {ensureDirSync} from 'https://deno.land/std@0.212.0/fs/ensure_dir.ts';
 export {default as MurmurHash3} from 'https://deno.land/x/murmurhash@v1.0.0/mod.ts';
-export * as svelte from 'npm:svelte@4.2.7/compiler';
-export * as acorn from 'npm:acorn@8.11.2';
+export * as svelte from 'npm:svelte@4.2.9/compiler';
+export * as acorn from 'npm:acorn@8.11.3';
